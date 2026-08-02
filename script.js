0b9cd824f06a75b25a95bab7e75b59b950e827bd
Agtk0209 <ahmtguray@gmail.com>
2026-08-02 21:44:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const alarmlar = {
+ 
 "STC1 F041": {
 uzunAdi: "Fault gap control right side",
 neden: "Gap sensörleri görüyor.",
@@ -7,26 +8,45 @@ mudahale: [
 "Sol sensörü kontrol et",
 "Fork'u hizala"
 ]
+},
+ 
+"F127": {
+uzunAdi: "Konveyör Motoru Aşırı Akım Alarmı",
+neden: "Motor sıkışması veya aşırı yük.",
+mudahale: [
+"Motoru durdur",
+"Mekanik sıkışmayı kontrol et",
+"Reset uygula"
+]
 }
+ 
 };
  
 function tarihSaat() {
+ 
 document.getElementById("saat").innerHTML =
 "📅 " + new Date().toLocaleString("tr-TR");
+ 
 }
  
 setInterval(tarihSaat, 1000);
 tarihSaat();
  
+let liste = "";
+ 
+for (let kod in alarmlar) {
+liste += `<option value="${kod}">`;
+}
+ 
 document.getElementById("alarmlarListesi").innerHTML =
-'<option value="STC1 F041">';
+liste;
  
 function alarmAra() {
  
-let alarmNo =
+const alarmNo =
 document.getElementById("alarmNo").value;
  
-let alarm =
+const alarm =
 alarmlar[alarmNo];
  
 if (!alarm) {
@@ -37,16 +57,26 @@ document.getElementById("sonuc").innerHTML =
 return;
 }
  
-document.getElementById("sonuc").innerHTML =
-`
+let html = `
 <h2>✅ Alarm Bulundu</h2>
  
 <p><b>Alarm No:</b> ${alarmNo}</p>
  
-<p><b>Uzun Adı:</b>
-${alarm.uzunAdi}</p>
+<p><b>Uzun Adı:</b> ${alarm.uzunAdi}</p>
  
-<p><b>Neden:</b>
-${alarm.neden}</p>
+<p><b>Neden:</b> ${alarm.neden}</p>
+ 
+<b>Müdahale Adımları</b>
+ 
+<ul>
 `;
+ 
+alarm.mudahale.forEach(adim => {
+html += `<li>${adim}</li>`;
+});
+ 
+html += "</ul>";
+ 
+document.getElementById("sonuc").innerHTML =
+html;
 }
