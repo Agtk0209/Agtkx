041879ac0c252517295b4b31b5d89d0a7ae334be
Agtk0209 <ahmtguray@gmail.com>
2026-08-02 21:39:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,27 @@
 const alarmlar = {
- 
-"STC1 F041":{
- 
-uzunAdi:
-"Fault gap control right side, check position of the bin - M41B7",
- 
-neden:
-"Fork bini üzerine aldıktan sonra güvenli bölgede değil. Gap sensörleri görüyor.",
- 
-mudahale:[
- 
+"STC1 F041": {
+uzunAdi: "Fault gap control right side",
+neden: "Gap sensörleri görüyor.",
+mudahale: [
 "Sağ sensörü kontrol et",
- 
 "Sol sensörü kontrol et",
- 
-"Reflektörleri temizle",
- 
-"Fork'u middle pozisyonuna getir",
- 
-"Robotu otomatiğe al",
- 
-"Start ver ve kontrol et"
-]
-},
- 
-"F127":{
- 
-uzunAdi:
-"Konveyör Motoru Aşırı Akım Alarmı",
- 
-neden:
-"Motor sıkışması veya aşırı yük.",
- 
-mudahale:[
- 
-"Motoru durdur",
- 
-"Mekanik sıkışmayı kontrol et",
- 
-"Motor sıcaklığını kontrol et",
- 
-"Reset uygula"
+"Fork'u hizala"
 ]
 }
 };
  
-function tarihSaat(){
- 
+function tarihSaat() {
 document.getElementById("saat").innerHTML =
-"📅 " +
-new Date().toLocaleString("tr-TR");
- 
+"📅 " + new Date().toLocaleString("tr-TR");
 }
  
-setInterval(tarihSaat,1000);
- 
+setInterval(tarihSaat, 1000);
 tarihSaat();
  
-for(let kod in alarmlar){
+document.getElementById("alarmlarListesi").innerHTML =
+'<option value="STC1 F041">';
  
-document.getElementById("alarmlarListesi")
-.innerHTML +=
-`<option value="${kod}">`;
- 
-}
- 
-function alarmAra(){
+function alarmAra() {
  
 let alarmNo =
 document.getElementById("alarmNo").value;
@@ -73,7 +29,7 @@ document.getElementById("alarmNo").value;
 let alarm =
 alarmlar[alarmNo];
  
-if(!alarm){
+if (!alarm) {
  
 document.getElementById("sonuc").innerHTML =
 "<h2>❌ Alarm Bulunamadı</h2>";
@@ -81,31 +37,16 @@ document.getElementById("sonuc").innerHTML =
 return;
 }
  
-let html = `
- 
+document.getElementById("sonuc").innerHTML =
+`
 <h2>✅ Alarm Bulundu</h2>
  
 <p><b>Alarm No:</b> ${alarmNo}</p>
  
-<p><b>Uzun Adı:</b><br>
+<p><b>Uzun Adı:</b>
 ${alarm.uzunAdi}</p>
  
-<p><b>Neden:</b><br>
+<p><b>Neden:</b>
 ${alarm.neden}</p>
- 
-<b>Müdahale Adımları</b>
- 
-<ul>
 `;
- 
-alarm.mudahale.forEach(adim=>{
- 
-html += `<li>${adim}</li>`;
- 
-});
- 
-html += "</ul>";
- 
-document.getElementById("sonuc").innerHTML =
-html;
 }
