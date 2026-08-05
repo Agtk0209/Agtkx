531c9c9955bf38fc80f4b47d418358d06b19ad25
Agtk0209 <ahmtguray@gmail.com>
2026-08-05 06:06:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
- 
-import {
-getFirestore,
-collection,
-getDocs,
-setDoc,
-doc
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
- 
-const firebaseConfig = {
-apiKey: "AIzaSyColf3uuecPo1lZa301yS9YoHNzIkdun4I",
-authDomain: "alarm-asistani.firebaseapp.com",
-projectId: "alarm-asistani",
-storageBucket: "alarm-asistani.firebasestorage.app",
-messagingSenderId: "80032270013",
-appId: "1:80032270013:web:7ef994f8345f3f2e6bd9d4",
-measurementId: "G-CC9554PJDY"
-};
- 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 let aktifAlarm = null;
 let toplamSorgu = 0;
 let alarmKayitlari = [];
@@ -452,12 +429,3 @@ ${alarmlar[kod].uzunAdi}
 document.getElementById("alarmListesi").innerHTML =
 html;
 }
-window.alarmAra = alarmAra;
-window.alarmEkle = alarmEkle;
-window.notKaydet = notKaydet;
-window.csvIndir = csvIndir;
-window.tumVerileriSil = tumVerileriSil;
-window.kayitliAlarmlariGoster = kayitliAlarmlariGoster;
-
-firebaseTest();
- 
